eca2b56a665cc5bf5ac531909fd30a364ef8bd8c
Pietro <Pietro@DESKTOP-NF1UQ89>
2021-01-09 18:30:04 +0100

    Commit di prova

--- TestGit/src/it/testgit/TestGit.java
+++ TestGit/src/it/testgit/TestGit.java
@@ -5,6 +5,12 @@ public class TestGit {
 	public static void main(String[] args) {
 
 		System.out.println("Test modifica altro account");
+		
+		//Prova modifica
+		
+		if(true) {
+			System.out.println("Ciao");
+		}
 
 	}
 
