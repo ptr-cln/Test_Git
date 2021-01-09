259a647367d2786b1ee84ed4317cdfc76ed62102
Pietro <Pietro@DESKTOP-NF1UQ89>
2021-01-09 12:24:59 +0100

    Test new Branch

--- TestGit/src/it/testgit/TestGit.java
+++ TestGit/src/it/testgit/TestGit.java
@@ -5,6 +5,9 @@ public class TestGit {
 	public static void main(String[] args) {
 
 		System.out.println("This is a stoopid test");
+		
+		
+		System.out.println("And this is the new branch!");
 
 	}
 
