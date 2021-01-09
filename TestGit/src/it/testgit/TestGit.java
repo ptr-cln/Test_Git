bac2d360b9246a0fb4f568aca25a2c873ef7995c
Pietro <Pietro@DESKTOP-NF1UQ89>
2021-01-09 12:54:32 +0100

    Test

--- TestGit/src/it/testgit/TestGit.java
+++ TestGit/src/it/testgit/TestGit.java
@@ -4,10 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is a stoopid test");
-		
-		
-		System.out.println("And this is the new branch!");
+		System.out.println("Test modifica");
 
 	}
 
