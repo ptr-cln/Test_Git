cf4790e999c0335482f9a01468c2c38b2e55b76e
Pietro <Pietro@DESKTOP-NF1UQ89>
2021-01-09 12:14:39 +0100

    Test

--- TestGit/src/it/testgit/TestGit.java
+++ TestGit/src/it/testgit/TestGit.java
@@ -3,7 +3,8 @@ package it.testgit;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("This is a stoopid test");
 
 	}
 
